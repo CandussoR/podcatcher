20a7261392a418486be1b9d5e55b372c7cd81951
Romain Candusso <r.candusso@protonmail.com>
2022-07-09 14:17:56 +0200

    Adds a displayName property

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <input type="text" required placeholder="Username" v-model="username" />
+    <input type="text" required placeholder="displayName" v-model="displayName" />
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
     <button>Sign Up</button>
@@ -17,17 +17,17 @@ export default {
 
     const email = ref("");
     const password = ref("");
-    const username = ref("");
+    const displayName = ref("");
 
     const handleSubmit = async () => {
-      await signUp(email.value, password.value, username.value);
+      await signUp(email.value, password.value, displayName.value);
       if (!error.value) {
         console.log("User signed up");
         context.emit("signUp");
       }
     };
 
-    return { email, password, username, handleSubmit, error };
+    return { email, password, displayName, handleSubmit, error };
   },
 };
 </script>
--- src/composables/useSignUp.js
+++ src/composables/useSignUp.js
@@ -4,14 +4,14 @@ import { ref } from "vue";
 
 const error = ref(null)
 
-const signUp = async (email, password, username) => {
+const signUp = async (email, password, displayName) => {
     error.value = null
     try {
         const res = await createUserWithEmailAndPassword(auth, email, password)
         if (!res) {
             throw new Error('Something went wrong')
         }
-        await updateProfile(auth.currentUser, { username })
+        await updateProfile(auth.currentUser, { displayName })
         error.value = null
 
         return res
