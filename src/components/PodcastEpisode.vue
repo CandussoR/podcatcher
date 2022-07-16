aff04e974c95ca9dac4f51b10577f4dbb3c03d7c
Romain Candusso <r.candusso@protonmail.com>
2022-07-16 15:45:55 +0200

    Adding dropdown menus and transitory css

--- src/components/PodcastEpisode.vue
+++ src/components/PodcastEpisode.vue
@@ -1,78 +1,93 @@
 <template>
-    <div class="episode-episode">
-        <img :src="episode.thumbnail"/>
-        <div class="details">
-            <p class="episode-title">{{ episode.title }}</p>
-            <p class="description">{{ descriptionSnippet }}</p>
-        </div>
-        <div class="action">
-            <div v-if="!reading || paused">
-                <span class="material-icons" @click="readAudio">play_arrow</span>
-            </div>
-            <div v-else>
-                <span class="material-icons" @click="pauseAudio">pause</span>
-            </div>
+  <div class="episode">
+    <img :src="episode.thumbnail" />
 
-        </div>
+    <div class="details">
+      <p class="episode-title">{{ episode.title }}</p>
+      <p class="description">{{ descriptionSnippet }}</p>
 
+      <!-- buttons -->
+      <div class="action">
+        <div v-if="!reading || paused">
+          <span class="material-icons" @click="readAudio">play_arrow</span>
+        </div>
+        <div v-else>
+          <span class="material-icons" @click="pauseAudio">pause</span>
+        </div>
+        <!-- dropDown playlist menu -->
+        <div class="dropdown">
+          <span class="material-icons">add</span>
+          <ul class="playlist-menu">
+            <li>New Playlist</li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
-import { ref } from 'vue'
-
+import { computed } from "@vue/runtime-core";
+import { ref } from "vue";
 
 export default {
-    props: ['episode'],
-    setup(props) {
-        const reading = ref(false);
-        const paused = ref(false);
-        const descriptionSnippet = computed(() => {
-            return props.episode.description.substring(0,100) + '...'
-        })
+  props: ["episode"],
+  setup(props) {
+    const reading = ref(false);
+    const paused = ref(false);
+    const descriptionSnippet = computed(() => {
+      return props.episode.description.substring(0, 100) + "...";
+    });
 
-        const audio = new Audio(props.episode.audio)
+    const audio = new Audio(props.episode.audio);
 
-        const readAudio = () => {
-            audio.play()
-            reading.value = true;
-        }
+    const readAudio = () => {
+      audio.play();
+      reading.value = true;
+    };
 
-        const pauseAudio = () => {
-            audio.pause()
-            reading.value = false;
-            paused.value = true;
-        }
+    const pauseAudio = () => {
+      audio.pause();
+      reading.value = false;
+      paused.value = true;
+    };
 
-        return { descriptionSnippet, readAudio, pauseAudio, reading, paused }
-    }
-}
+    return { descriptionSnippet, readAudio, pauseAudio, reading, paused };
+  },
+};
 </script>
 
-<style>
-    .episode-episode {
-        display: flex;
-        max-height: 200px;
-        background-color: #FB8574;
-        padding: 10px;
-        border-bottom: 1px solid #171E2B;
-        border-radius: 20px;
-    }
-    .details {
-        display: flex;
-        flex-direction: column;
-        padding: 10px;
-    }
-    .episode-episode p.episode-title {
-        font-weight: bold;
-    }
-    img {
-        max-width: 80px;
-        max-height: 80px;
-    }
-    .action {
-        display: flex;
-        justify-content: space-between;
-    }
+<style scoped>
+.episode {
+  display: flex;
+  max-height: 200px;
+  background-color: #fb8574;
+  padding: 10px;
+  border-bottom: 1px solid #171e2b;
+  border-radius: 20px;
+  margin-bottom: 5px;
+}
+.details {
+  display: block;
+  flex-direction: column;
+  padding: 10px;
+}
+.episode-episode p.episode-title {
+  font-weight: bold;
+}
+img {
+  max-width: 80px;
+  max-height: 80px;
+}
+.action {
+  display: flex;
+  justify-content: space-between;
+}
+.dropdown:active .playlist-menu {
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  padding-top: 5px;
+  position: absolute;
+}
 </style>
--- src/components/SearchEpisode.vue
+++ src/components/SearchEpisode.vue
@@ -1,75 +1,109 @@
 <template>
-    <div class="episode-result">
-        <img :src="result.thumbnail" />
-        <div class="details">
-            <p class="episode-title">{{ result.title_original }}</p>
-            <p class="description">{{ descriptionSnippet }}</p>
-        </div>
-        <div class="action">
-            <div v-if="!reading">
-                <span class="material-icons" @click="readAudio">play_arrow</span>
-            </div>
-            <div v-else>
-                <span class="material-icons" @click="pauseAudio">pause</span>
-            </div>
+  <div class="episode-result">
+    <img :src="result.thumbnail" />
 
+    <div class="details">
+      <p class="episode-title">{{ result.title_original }}</p>
+      <p class="description">{{ descriptionSnippet }}</p>
+
+      <!-- buttons -->
+      <div class="action">
+        <div v-if="!reading || paused">
+          <span class="material-icons" @click="readAudio">play_arrow</span>
         </div>
+        <div v-else>
+          <span class="material-icons" @click="pauseAudio">pause</span>
+        </div>
+
+        <!-- dropDown playlist menu -->
+        <div class="dropdown">
+          <span class="material-icons" @click="showMenu = !showMenu">add</span>
+          <ul v-if="showMenu" class="playlist-menu">
+            <li>New Playlist</li>
+            <li>Second</li>
+          </ul>
+        </div>
+      </div>
 
     </div>
+    
+  </div>
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
-import { ref } from 'vue'
+import { computed } from "@vue/runtime-core";
+import { ref } from "vue";
 
 export default {
-    props: ['result'],
-    setup(props) {
-        const reading = ref(false);
-        const descriptionSnippet = computed(() => {
-            return props.result.description_original.substring(0,100) + '...'
-        })
+  props: ["result"],
+  setup(props) {
+    const reading = ref(false);
+    const isPaused = ref(false);
+    const showMenu = ref(false);
 
-        const audio = new Audio(props.result.audio)
+    const descriptionSnippet = computed(() => {
+      return props.result.description_original.substring(0, 100) + "...";
+    });
 
-        const readAudio = () => {
-            audio.play()
-            reading.value = true;
-        }
+    const audio = new Audio(props.result.audio);
 
-        const pauseAudio = () => {
-            audio.pause()
-            reading.value = false;
-        }
+    const readAudio = () => {
+      audio.play();
+      reading.value = true;
+    };
 
-        return { descriptionSnippet, readAudio, pauseAudio, reading }
-    }
-}
+    const pauseAudio = () => {
+      audio.pause();
+      reading.value = false;
+      isPaused.value = true;
+    };
+
+    return { descriptionSnippet, readAudio, pauseAudio, reading, isPaused, showMenu };
+  },
+};
 </script>
 
 <style>
-    .episode-result {
-        display: flex;
-        max-height: 200px;
-        background-color: #FB8574;
-        padding: 10px;
-        border-bottom: 1px solid #171E2B;
-        border-radius: 20px;
-    }
-    .details {
-        display: flex;
-        flex-direction: column;
-        background-color: #F7FBFF;
-    }
-    .episode-result p.episode-title {
-        font-weight: bold;
-    }
-    img {
-        max-width: 80px;
-        max-height: 80px;
-    }
-    .action {
-        display: flex;
-        justify-content: space-between;
-    }
+.episode-result {
+  display: flex;
+  max-height: 200px;
+  background-color: #fb8574;
+  padding: 10px;
+  border-bottom: 1px solid #171e2b;
+  border-radius: 20px;
+}
+.details {
+  display: flex;
+  flex-direction: column;
+  background-color: #f7fbff;
+}
+.episode-result p.episode-title {
+  font-weight: bold;
+}
+img {
+  max-width: 80px;
+  max-height: 80px;
+}
+.action {
+  display: flex;
+}
+.dropdown {
+  position: relative;
+  top: 0 px;
+  left: 20px;
+}
+.playlist-menu {
+  position: absolute;
+  background: turquoise;
+  border-radius: 10px;
+  max-width: max-content;
+  white-space: nowrap;
+}
+.playlist-menu li {
+  list-style: none;
+  text-align: left;
+}
+li:hover {
+  background: grey;
+}
 </style>
