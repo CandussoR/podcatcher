61039465866caceb12bb56c1bf55b8a59aa4d14d
Romain Candusso <r.candusso@protonmail.com>
2022-07-28 11:40:18 +0200

    Simplifying the structure by removing content of Login and SignUp forms in the views

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,44 @@
 <template>
   <h1>Login</h1>
-  <LoginForm @login="welcomeUser" />
+
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <div v-if="error">{{ error }}</div>
+    <button>Login</button>
+  </form>
+
   <p>You don't have an account ?</p>
   <button @click="toSignUp">Sign up!</button>
 </template>
 
 <script>
-import LoginForm from "@/components/LoginForm.vue";
 import { useRouter } from "vue-router";
+import { ref } from "@vue/reactivity";
+import useLogin from "@/composables/useLogin";
 
 export default {
-  components: { LoginForm },
   setup() {
+    const email = ref("");
+    const password = ref("");
+
     const router = useRouter();
 
-    const welcomeUser = () => {
-      router.push({ name: "home" });
+    const { error, login } = useLogin();
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value);
+      if (!error.value) {
+        console.log("User logged in");
+        router.push({ name: "home" });
+      }
     };
 
     const toSignUp = () => {
       router.push({ name: "signUp" });
     };
 
-    return { welcomeUser, toSignUp };
+    return { email, password, handleSubmit, toSignUp, error };
   },
 };
 </script>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,27 +1,45 @@
 <template>
   <h1>Sign Up</h1>
-  <SignUpForm @signUp="welcomeUser" />
+
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="displayName" v-model="displayName" />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign Up</button>
+  </form>
+
   <p>Already have an account ?</p>
   <button @click="toLogin">Login</button>
 </template>
 
 <script>
-import SignUpForm from "@/components/SignUpForm.vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import useSignUp from "@/composables/useSignUp";
 export default {
-  components: { SignUpForm },
   setup() {
+    // refs
+    const email = ref("");
+    const password = ref("");
+    const displayName = ref("");
+
     const router = useRouter();
 
-    const welcomeUser = () => {
-      router.push({ name: "home" });
+    const { error, signUp } = useSignUp();
+
+    const handleSubmit = async () => {
+      await signUp(email.value, password.value, displayName.value);
+      if (!error.value) {
+        console.log("User signed up");
+        router.push({ name: "home" });
+      }
     };
 
     const toLogin = () => {
       router.push({ name: "login" });
     };
 
-    return { welcomeUser, toLogin };
+    return { email, password, displayName, handleSubmit, toLogin };
   },
 };
 </script>
