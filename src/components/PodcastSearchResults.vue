437bf8245c591cf90642ea7db7086d8d36454d62
Romain Candusso <r.candusso@protonmail.com>
2022-07-30 11:27:14 +0200

    Moved code from component SearchPodcast into SearchPodcastResults for simplicity

--- src/components/PodcastSearchResults.vue
+++ src/components/PodcastSearchResults.vue
@@ -1,24 +1,49 @@
 <template>
   <div class="results">
     <div v-for="result in searchResults" :key="result">
-      <SearchPodcast :result="result" />
+      <div class="podcast-result">
+        <img :src="result.thumbnail" />
+        <router-link :to="{ name: 'podcast', params: { id: result.id } }">
+          <p class="podcast-title">{{ result.title_original }}</p>
+        </router-link>
+        <p class="description">{{ snippet }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import SearchPodcast from './SearchPodcast.vue'
+import { computed } from "@vue/runtime-core";
 export default {
-    props: ["searchResults"],
-    components: { SearchPodcast }
-}
+  props: ["searchResults"],
+  setup(props) {
+    console.log(props.searchResults);
+    const snippet = computed(() => {
+      return props.searchResults.forEach(
+        (pod) => pod.description_original.substring(0, 100) + "..."
+      );
+    });
+
+    return { snippet };
+  },
+};
 </script>
 
 <style>
-  .results {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: #F7FBFF;
-  }
+.results {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #f7fbff;
+}
+.podcast-result {
+  max-height: 200px;
+  background-color: #fefef3;
+}
+.podcast-result p.podcast-title {
+  font-weight: bold;
+}
+img {
+  max-width: 80px;
+}
 </style>
